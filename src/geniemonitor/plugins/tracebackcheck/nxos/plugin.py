94b7602abfb6e2328d767688eacaddc3d74f72ca
Lubna Rasheed <lrasheed@cisco.com>
2017-10-12 12:46:49 -0700

    Geniemonitor: redesign plugin layout

--- src/geniemonitor/plugins/tracebackcheck/nxos/plugin.py
+++ src/geniemonitor/plugins/tracebackcheck/nxos/plugin.py
@@ -2,70 +2,11 @@
 GenieMonitor Traceback Check Plugin for NXOS.
 '''
 
-# Python
-import re
-import time
-import logging
-from datetime import datetime
-
 # GenieMonitor
 from ..plugin import Plugin as BasePlugin
-from geniemonitor.results import OK, WARNING, ERRORED, PARTIAL
-
-# module logger
-logger = logging.getLogger(__name__)
 
 
 class Plugin(BasePlugin):
 
-    def execution(self, device, execution_time):
-
-        # Check device is connected
-        if not device.is_connected():
-            return ERRORED
-
-        # Init
-        status_= OK
-        traceback_found = False
-        # Default keywords: 'Traceback', 'ERROR' & 'WARNING'
-        default_include = ['Traceback', 'ERROR', 'WARNING']
-
-        # Execute 'show logging logfile'
-        output = device.execute('show logging logfile')
-        if not output:
-            return ERRORED
-
-        # Parse 'show logging logfile' output for keywords in include_pattern
-        # Omit keywords specified in exclude_pattern
-        include = self.args.include_pattern + default_include
-
-        # Final list of keywords to check
-        for item in include:
-            if item in self.args.exclude_pattern:
-                include.remove(item)
-
-        # Parse each line for keywords
-        for line in output.splitlines():
-            for pattern in include:
-                if re.search(pattern, line.strip(), re.IGNORECASE):
-                    traceback_found = True
-                    logger.error("\nFound pattern '{pattern}' in"
-                                 " 'show logging logfile' output".\
-                                 format(pattern=pattern))
-
-        # Log message to user
-        if not traceback_found:
-            logger.info("\n\n***** No traceback patterns matched *****\n\n")
-
-        # Clear logging (if user specified)
-        if self.args.clean_up:
-            try:
-                device.execute('clear logging logfile')
-            except Exception as e:
-                logger.error("\nClear logging execution failed")
-                status_ += ERRORED
-
-        # Final status
-        data = dict(object = device.name, status = status_, result = output)
-        self.generate_result_meta(now = execution_time, **data)
-        return status_
+    show_cmd = 'show logging logfile'
+    clear_cmd = 'clear logging logfile'
--- src/geniemonitor/plugins/tracebackcheck/plugin.py
+++ src/geniemonitor/plugins/tracebackcheck/plugin.py
@@ -2,12 +2,24 @@
 GenieMonitor Traceback Check Plugin.
 '''
 
+# Python
+import re
+import time
+import logging
+from datetime import datetime
+
 # ATS
 from ats.utils import parser as argparse
 from ats.datastructures import classproperty
 
 # GenieMonitor
+#from ..plugin import Plugin as BasePlugin
 from geniemonitor.plugins.bases import BasePlugin
+from geniemonitor.results import OK, WARNING, ERRORED, PARTIAL
+
+# module logger
+logger = logging.getLogger(__name__)
+
 
 class Plugin(BasePlugin):
 
@@ -43,3 +55,56 @@ class Plugin(BasePlugin):
                                  'tracebacks after reporting error')
 
         return parser
+
+
+    def execution(self, device, execution_time):
+
+        # Check device is connected
+        if not device.is_connected():
+            return ERRORED
+
+        # Init
+        status_= OK
+        traceback_found = False
+        # Default keywords
+        default_include = ['Traceback', 'ERROR', 'WARNING']
+
+        # Execute command to check for tracebacks - timeout set to 5 mins
+        output = device.execute(self.show_cmd, timeout=300)
+        if not output:
+            return ERRORED
+
+        # Parse 'show logging logfile' output for keywords in include_pattern
+        # Omit keywords specified in exclude_pattern
+        include = self.args.include_pattern + default_include
+
+        # Final list of keywords to check
+        for item in include:
+            if item in self.args.exclude_pattern:
+                include.remove(item)
+
+        # Parse each line for keywords
+        for line in output.splitlines():
+            for pattern in include:
+                if re.search(pattern, line.strip(), re.IGNORECASE):
+                    traceback_found = True
+                    logger.error("\nFound pattern '{pattern}' in"
+                                 " 'show logging logfile' output".\
+                                 format(pattern=pattern))
+
+        # Log message to user
+        if not traceback_found:
+            logger.info("\n\n***** No traceback patterns matched *****\n\n")
+
+        # Clear logging (if user specified)
+        if self.args.clean_up:
+            try:
+                device.execute(self.clear_cmd)
+            except Exception as e:
+                logger.error("\nClear logging execution failed")
+                status_ += ERRORED
+
+        # Final status
+        data = dict(object = device.name, status = status_, result = output)
+        self.generate_result_meta(now = execution_time, **data)
+        return status_
