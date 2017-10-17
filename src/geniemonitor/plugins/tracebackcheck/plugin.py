cd527171a05f3d72da658e93144a539c02d8a61b
Lubna Rasheed <lrasheed@cisco.com>
2017-10-17 07:39:01 -0700

    Geniemonitor: tracebackcheck plugin - addressed redesign comments

--- src/geniemonitor/plugins/tracebackcheck/plugin.py
+++ src/geniemonitor/plugins/tracebackcheck/plugin.py
@@ -4,17 +4,15 @@ GenieMonitor Traceback Check Plugin.
 
 # Python
 import re
-import time
 import logging
-from datetime import datetime
 
 # ATS
+from ats.log.utils import banner
 from ats.utils import parser as argparse
 from ats.datastructures import classproperty
-from ats.log.utils import banner
+from ats.datastructures.logic import logic_str
 
 # GenieMonitor
-#from ..plugin import Plugin as BasePlugin
 from geniemonitor.plugins.bases import BasePlugin
 from geniemonitor.results import OK, WARNING, ERRORED, PARTIAL, CRITICAL
 
@@ -31,21 +29,13 @@ class Plugin(BasePlugin):
         parser = argparse.ArgsPropagationParser(add_help = False)
         parser.title = 'Traceback Check'
         
-        # include_pattern args
-        # --------------------
-        parser.add_argument('-include_pattern',
+        # logic_pattern
+        # -------------
+        parser.add_argument('-logic_pattern',
                             action="store",
                             default='',
-                            help='Specify which patterns to include when '
-                                 'checking tracebacks')
-
-        # exclude_pattern args
-        # --------------------
-        parser.add_argument('-exclude_pattern',
-                            action="store",
-                            default='',
-                            help='Specify which patterns to exclude when '
-                                 'checking tracebacks')
+                            help='Specify logical expression for patterns to '
+                                 ' include/exclude when checking tracebacks')
 
         # clean_up
         # --------
@@ -70,41 +60,30 @@ class Plugin(BasePlugin):
 
         # Init
         status_= OK
-        matched_patterns_dict = {}
-        # Default keywords
-        default_include = ['Traceback', 'ERROR', 'WARNING']
+        matched_lines_dict = {}
 
         # Execute command to check for tracebacks - timeout set to 5 mins
         output = device.execute(self.show_cmd, timeout=self.args.timeout)
         if not output:
             return ERRORED
 
-        # Parse 'show logging logfile' output for keywords in include_pattern
-        # Omit keywords specified in exclude_pattern
-        include = self.args.include_pattern.split(', ') + default_include
+        # Logic pattern
+        match_patterns = logic_str(self.args.logic_pattern)
 
-        # Final list of keywords to check
-        for item in include:
-            if item in self.args.exclude_pattern.split(', '):
-                include.remove(item)
-
-        # Parse each line for keywords
-        for pattern in include:
-            matched_lines = []
-            for line in output.splitlines():
-                if re.search(pattern, line.strip(), re.IGNORECASE):
-                    if 'patterns' not in matched_patterns_dict:
-                        matched_patterns_dict['patterns'] = {}
-                    if pattern not in matched_patterns_dict:
-                        matched_patterns_dict['patterns'][pattern] = {}
-                    matched_lines.append(line)
-                    matched_patterns_dict['patterns'][pattern] = matched_lines
-                    status_ += CRITICAL
-                    logger.error(banner("\nMatched pattern '{pattern}' in line:\n'{line}'".\
-                                 format(pattern=pattern, line=line)))
+        # Parse 'show logging logfile' output for keywords
+        matched_lines = []
+        for line in output.splitlines():
+            if match_patterns:
+                if 'matched_lines' not in matched_lines_dict:
+                    matched_lines_dict['matched_lines'] = {}
+                matched_lines.append(line)
+                matched_lines_dict['matched_lines'] = matched_lines
+                status_ += CRITICAL
+                logger.error(banner("\nMatched pattern in line:\n'{line}'".\
+                             format(line=line)))
 
         # Log message to user
-        if not matched_patterns_dict:
+        if not matched_lines_dict:
             logger.info(banner("\n\n***** No traceback patterns matched *****\n\n"))
 
         # Clear logging (if user specified)
@@ -116,6 +95,6 @@ class Plugin(BasePlugin):
                 status_ += ERRORED
 
         # Final status
-        data = dict(device = device.name, status = status_, traceback = matched_patterns_dict)
+        data = dict(device = device.name, status = status_, error_patterns = matched_lines_dict)
         self.generate_result_meta(**data)
         return status_
