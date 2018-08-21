7cfe90a68fc542357277410d5d8d93326bc82e57
Wei Chen <weiche3@cisco.com>
2018-08-21 16:18:41 -0400

    only rollup what is needed

--- src/genie/telemetry/status/utils.py
+++ src/genie/telemetry/status/utils.py
@@ -50,7 +50,7 @@ def massage_meta(input_, syntax = None):
             break
     else:
         # all keys are datetime string
-        return syntax_converter(input_, supported_syntax)
+        return intput_
     # wrap with datetime key
     return { key: syntax_converter(input_, supported_syntax) }
 
