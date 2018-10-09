2458fe8e9ebdb744044891ff1caaf509842c1803
karmoham <karmoham@cisco.com>
2018-10-09 16:47:53 -0400

    Increasing version number for release

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -1,5 +1,5 @@
 # metadata
-__version__ = '3.0.2'
+__version__ = '3.1.0'
 __author__ = 'ASG/ATS Team'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
