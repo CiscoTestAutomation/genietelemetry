4ceedcb15a6537b6621a293903fd6f34b249b507
Siming Yuan <siyuan@cisco.com>
2018-10-25 16:44:12 -0400

    - ces monitor code update

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -1,5 +1,5 @@
 # metadata
-__version__ = '3.1.0'
+__version__ = '3.1.1'
 __author__ = 'ASG/ATS Team'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
@@ -8,12 +8,22 @@ from .main import main
 from .plugin import BasePlugin
 from .manager import Manager, TimedManager
 
+# try to record usage statistics
+#  - only internal cisco users will have stats.CesMonitor module
+#  - below code does nothing for DevNet users -  we DO NOT track usage stats
+#    for PyPI/public/customer users
 try:
+    # new internal cisco-only pkg since devnet release
     from ats.cisco.stats import CesMonitor
-    CesMonitor(action = 'genietelemetry', application='Genie').post()
 except Exception:
     try:
+        # legacy pyats version, stats was inside utils module
         from ats.utils.stats import CesMonitor
-        CesMonitor(action = 'genietelemetry', application='Genie').post()
     except Exception:
-        pass
+        CesMonitor = None
+
+finally:
+    if CesMonitor is not None:
+        # CesMonitor exists -> this is an internal cisco user
+        CesMonitor(action = __name__, application='Genie').post()
+        CesMonitor(action = __name__, application='pyATS Packages').post()
