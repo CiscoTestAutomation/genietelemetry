58efcb793abe24ecf1cba765d2e5059d8b890cad
karmoham <karmoham@cisco.com>
2019-05-27 03:37:32 -0400

    Releasing package version 19.5.0

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -1,5 +1,5 @@
 # metadata
-__version__ = '19.4.0'
+__version__ = '19.5.0'
 __author__ = 'ASG/ATS Team'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
