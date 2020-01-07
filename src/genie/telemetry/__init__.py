e236d5216f702bbd29ff3b41331ed858394467ee
Siming Yuan <siyuan@cisco.com>
2020-01-07 11:58:24 -0500

    ces monitor decomissioned

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -7,23 +7,3 @@ __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
 from .main import main
 from .plugin import BasePlugin
 from .manager import Manager, TimedManager
-
-# try to record usage statistics
-#  - only internal cisco users will have stats.CesMonitor module
-#  - below code does nothing for DevNet users -  we DO NOT track usage stats
-#    for PyPI/public/customer users
-try:
-    # new internal cisco-only pkg since devnet release
-    from pyats.cisco.stats import CesMonitor
-except Exception:
-    try:
-        # legacy pyats version, stats was inside utils module
-        from pyats.utils.stats import CesMonitor
-    except Exception:
-        CesMonitor = None
-
-finally:
-    if CesMonitor is not None:
-        # CesMonitor exists -> this is an internal cisco user
-        CesMonitor(action = __name__, application='Genie').post()
-        CesMonitor(action = __name__, application='pyATS Packages').post()
