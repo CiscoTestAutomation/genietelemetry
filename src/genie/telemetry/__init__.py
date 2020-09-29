9848c3ce1e6b1354a894e48450d845546b44c6a3
Lunzhi Dong -X (lundong - ALBANY SERVICES  INC at Cisco) <lundong@cisco.com>
2020-09-29 18:24:26 -0400

    releasing v20.9

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -1,5 +1,5 @@
 # metadata
-__version__ = '20.8'
+__version__ = '20.9'
 __author__ = 'ASG/ATS Team'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
