d4ab082f40df6ace1990c237014e3172b25a00a4
LIAM GERRIOR -X (lgerrior - HIGH TECH GENESIS INC at Cisco) <lgerrior@cisco.com>
2021-10-25 09:34:50 -0400

    bump genie version 21.8 -> 21.10

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -1,5 +1,5 @@
 # metadata
-__version__ = '21.8'
+__version__ = '21.10'
 __author__ = 'ASG/ATS Team'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
