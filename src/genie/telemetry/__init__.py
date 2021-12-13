52a8f59545a030ed538e107117fb5c7b9444e7d0
Lukas McClelland <lumcclel@cisco.com>
2021-12-13 10:29:17 -0500

    bump genie version 21.10 -> 21.12

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -1,5 +1,5 @@
 # metadata
-__version__ = '21.10'
+__version__ = '21.12'
 __author__ = 'ASG/ATS Team'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
