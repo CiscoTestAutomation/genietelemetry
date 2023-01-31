b6b72639503d160168500f7c619a651eda35a4d3
GerriorL <84335026+gerriorl@users.noreply.github.com>
2023-01-31 11:05:25 -0500

    Releasing v23.1

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -1,5 +1,5 @@
 # metadata
-__version__ = '22.11'
+__version__ = '23.1'
 __author__ = 'ASG/ATS Team'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
