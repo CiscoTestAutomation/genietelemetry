0ab51f2e75bed7ad48ebdf2e7c30bfab74f7b707
Thomas Ryan <thomryan@cisco.com>
2024-10-22 13:39:29 -0400

    Releasing v24.10

--- src/genie/telemetry/__init__.py
+++ src/genie/telemetry/__init__.py
@@ -1,5 +1,5 @@
 # metadata
-__version__ = '24.9'
+__version__ = '24.10'
 __author__ = 'ASG/ATS Team'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
